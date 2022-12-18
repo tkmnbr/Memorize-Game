55bb3ade2fa0d267445b4d0a9c47f927fc899ca3
Noboru Tokimi <tkmnbrgit@gmail.com>
2022-12-18 12:01:08 -0800

    Initial Commit

---
+++ Memorize Game/Memorize_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_GameApp.swift
+//  Memorize Game
+//
+//  Created by Noboru Tokimi on 12/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
