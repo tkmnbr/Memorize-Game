113e1c04b264477cc45a3b6339555b1e6ea66166
Noboru Tokimi <tkmnbrgit@gmail.com>
2022-12-18 13:11:29 -0800

    added rectangle view

--- Memorize Game/ContentView.swift
+++ Memorize Game/ContentView.swift
@@ -9,15 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3.0)
+            Text("Hello, Noboru!")
+                .foregroundColor(Color.black)
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.purple/*@END_MENU_TOKEN@*/)
+
+    
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
